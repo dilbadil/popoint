906debfd02956b84373c773b2c89a6b27d6e4036
dilbadil <abdillah308@gmail.com>
2015-04-19 09:56:25 +0700

    bismillah

---
+++ app/Http/Controllers/Admins/DashboardController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers\Admins;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller {
+
+	//
+
+}
---
+++ app/Http/Controllers/ControlPanels/ControlPanelController.php
@@ -0,0 +1,12 @@
+<?php namespace App\Http\Controllers\ControlPanels;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class ControlPanelController extends Controller {
+
+	//
+
+}
